6735d97dc71e51f91f8100232323e3ed54e9374f
ldm0 <ldm2993593805@163.com>
2026-07-14 06:09:37 +0800

    fix(renderer): complete tabular element reflection

--- moli-renderer-v8/src/native_bridge/element/reflection.rs
+++ moli-renderer-v8/src/native_bridge/element/reflection.rs
@@ -241,6 +241,9 @@ pub(super) enum DomStringReflection {
     StyleMedia,
     StyleType,
     TableBorder,
+    TableFrame,
+    TableRules,
+    TableSummary,
     TableCellAbbr,
     TableCellAxis,
     TableCellCh,
@@ -693,6 +696,33 @@ const DOM_STRING_REFLECTION_DESCRIPTORS: &[(DomStringReflection, DomStringReflec
             DomStringReflectionDescriptor::new("HTMLTableElement", "border", "border"),
         ),
         (
+            DomStringReflection::TableFrame,
+            DomStringReflectionDescriptor::new_html_element(
+                "HTMLTableElement",
+                "table",
+                "frame",
+                "frame",
+            ),
+        ),
+        (
+            DomStringReflection::TableRules,
+            DomStringReflectionDescriptor::new_html_element(
+                "HTMLTableElement",
+                "table",
+                "rules",
+                "rules",
+            ),
+        ),
+        (
+            DomStringReflection::TableSummary,
+            DomStringReflectionDescriptor::new_html_element(
+                "HTMLTableElement",
+                "table",
+                "summary",
+                "summary",
+            ),
+        ),
+        (
             DomStringReflection::TableCellAbbr,
             DomStringReflectionDescriptor::new("HTMLTableCellElement", "abbr", "abbr"),
         ),
@@ -887,6 +917,8 @@ pub(super) enum NullToEmptyDomStringReflection {
     ImageBorder,
     ObjectBorder,
     TableBgColor,
+    TableCellPadding,
+    TableCellSpacing,
     TableCellBgColor,
     TableRowBgColor,
     Count,
@@ -931,6 +963,24 @@ const NULL_TO_EMPTY_DOM_STRING_REFLECTION_DESCRIPTORS: &[(
     (
         NullToEmptyDomStringReflection::TableBgColor,
         ReflectedAttributeDescriptor::new("HTMLTableElement", "bgcolor", "bgColor"),
+    ),
+    (
+        NullToEmptyDomStringReflection::TableCellPadding,
+        ReflectedAttributeDescriptor::new_html_element(
+            "HTMLTableElement",
+            "table",
+            "cellpadding",
+            "cellPadding",
+        ),
+    ),
+    (
+        NullToEmptyDomStringReflection::TableCellSpacing,
+        ReflectedAttributeDescriptor::new_html_element(
+            "HTMLTableElement",
+            "table",
+            "cellspacing",
+            "cellSpacing",
+        ),
     ),
     (
         NullToEmptyDomStringReflection::TableCellBgColor,
