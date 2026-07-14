ffe5ec2512cf93c596bfe80e0fd095c832ec2417
ldm0 <ldm2993593805@163.com>
2026-07-14 22:12:47 +0800

    fix(html): expose document named properties

--- moli-renderer-v8/src/native_bridge/identity.rs
+++ moli-renderer-v8/src/native_bridge/identity.rs
@@ -173,6 +173,7 @@ pub(super) enum LiveCollectionQueryKind {
     ClassName,
     Name,
     WindowNamedItems,
+    DocumentNamedItems,
     DocumentAllNamedItems,
     FormControlsByName,
     Forms,
@@ -202,6 +203,7 @@ impl LiveCollectionQueryKind {
             Self::ClassName => "className",
             Self::Name => "name",
             Self::WindowNamedItems => "windowNamedItems",
+            Self::DocumentNamedItems => "documentNamedItems",
             Self::DocumentAllNamedItems => "documentAllNamedItems",
             Self::FormControlsByName => "formControlsByName",
             Self::Forms => "forms",
@@ -316,6 +318,11 @@ impl LiveCollectionDescriptor {
             crate::native_bridge::named_access::window_named_item_handles(
                 host.dom_host(),
                 self.root,
+                self.query.as_deref().unwrap_or_default(),
+            )
+        } else if self.query_kind == LiveCollectionQueryKind::DocumentNamedItems {
+            crate::native_bridge::named_access::document_named_item_handles(
+                host.dom_host(),
                 self.query.as_deref().unwrap_or_default(),
             )
         } else if self.query_kind == LiveCollectionQueryKind::DocumentAllNamedItems {
