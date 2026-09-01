2569983194f2ebc453179f4deeb72fd5bb5a5cc8
ldm0 <ldm2993593805@163.com>
2026-09-01 17:20:08 +0800

    fix(url): expose colon protocol for invalid hyperlinks

--- moli-renderer-v8/src/native_bridge/element.rs
+++ moli-renderer-v8/src/native_bridge/element.rs
@@ -2081,6 +2081,7 @@ struct HtmlTableElementPrototypeDeclaration {
 fn anchor_url_string_function_getter<'s>(
     scope: &mut v8::PinScope<'s, '_>,
     receiver: v8::Local<'s, v8::Object>,
+    parse_failure_value: &'static str,
     project: impl FnOnce(&url::Url) -> String,
     mut rv: v8::ReturnValue<'_, v8::Value>,
 ) {
@@ -2092,7 +2093,7 @@ fn anchor_url_string_function_getter<'s>(
     };
     let value = parsed_url_like_attribute(unsafe { &*runtime_ptr }, handle, "href")
         .map(|url| project(&url))
-        .unwrap_or_default();
+        .unwrap_or_else(|| parse_failure_value.to_owned());
     if let Some(value) = v8_string(scope, &value) {
         rv.set(value.into());
     } else {
@@ -2358,6 +2359,7 @@ fn anchor_host_getter_function<'s>(
     anchor_url_string_function_getter(
         scope,
         args.this(),
+        "",
         |url| {
             url.host_str()
                 .map(|host| {
@@ -2414,6 +2416,7 @@ fn anchor_hostname_getter_function<'s>(
     anchor_url_string_function_getter(
         scope,
         args.this(),
+        "",
         |url| url.host_str().unwrap_or_default().to_owned(),
         rv,
     );
@@ -2446,6 +2449,7 @@ fn anchor_port_getter_function<'s>(
     anchor_url_string_function_getter(
         scope,
         args.this(),
+        "",
         |url| url.port().map(|port| port.to_string()).unwrap_or_default(),
         rv,
     );
@@ -2475,7 +2479,7 @@ fn anchor_pathname_getter_function<'s>(
     args: v8::FunctionCallbackArguments<'s>,
     rv: v8::ReturnValue<'_, v8::Value>,
 ) {
-    anchor_url_string_function_getter(scope, args.this(), |url| url.path().to_owned(), rv);
+    anchor_url_string_function_getter(scope, args.this(), "", |url| url.path().to_owned(), rv);
 }
 
 fn anchor_pathname_setter_function<'s>(
@@ -2504,6 +2508,7 @@ fn anchor_search_getter_function<'s>(
     anchor_url_string_function_getter(
         scope,
         args.this(),
+        "",
         |url| url::quirks::search(url).to_owned(),
         rv,
     );
@@ -2532,6 +2537,7 @@ fn anchor_hash_getter_function<'s>(
     anchor_url_string_function_getter(
         scope,
         args.this(),
+        "",
         |url| url::quirks::hash(url).to_owned(),
         rv,
     );
@@ -2557,7 +2563,13 @@ fn anchor_origin_getter_function<'s>(
     args: v8::FunctionCallbackArguments<'s>,
     rv: v8::ReturnValue<'_, v8::Value>,
 ) {
-    anchor_url_string_function_getter(scope, args.this(), moli_url::origin_ascii_serialization, rv);
+    anchor_url_string_function_getter(
+        scope,
+        args.this(),
+        "",
+        moli_url::origin_ascii_serialization,
+        rv,
+    );
 }
 
 fn anchor_protocol_getter_function<'s>(
@@ -2565,7 +2577,13 @@ fn anchor_protocol_getter_function<'s>(
     args: v8::FunctionCallbackArguments<'s>,
     rv: v8::ReturnValue<'_, v8::Value>,
 ) {
-    anchor_url_string_function_getter(scope, args.this(), |url| format!("{}:", url.scheme()), rv);
+    anchor_url_string_function_getter(
+        scope,
+        args.this(),
+        ":",
+        |url| format!("{}:", url.scheme()),
+        rv,
+    );
 }
 
 fn anchor_protocol_setter_function<'s>(
@@ -2589,7 +2607,7 @@ fn anchor_username_getter_function<'s>(
     args: v8::FunctionCallbackArguments<'s>,
     rv: v8::ReturnValue<'_, v8::Value>,
 ) {
-    anchor_url_string_function_getter(scope, args.this(), |url| url.username().to_owned(), rv);
+    anchor_url_string_function_getter(scope, args.this(), "", |url| url.username().to_owned(), rv);
 }
 
 fn anchor_username_setter_function<'s>(
@@ -2618,6 +2636,7 @@ fn anchor_password_getter_function<'s>(
     anchor_url_string_function_getter(
         scope,
         args.this(),
+        "",
         |url| url.password().unwrap_or("").to_owned(),
         rv,
     );
