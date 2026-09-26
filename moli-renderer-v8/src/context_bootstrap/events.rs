Stravia-AI/moli-stealth

fix(html): align command interface semantics

moli-renderer-v8/src/context_bootstrap/events.rs
@@ -14,6 +14,8 @@ const CLOSE_EVENT_REASON_SLOT: &str = "__moliCloseEventReason";
 const SUBMIT_EVENT_SUBMITTER_SLOT: &str = "__moliSubmitEventSubmitter";
 const FORM_DATA_EVENT_FORM_DATA_SLOT: &str = "__moliFormDataEventFormData";
 const TRACK_EVENT_TRACK_SLOT: &str = "__moliTrackEventTrack";
+const COMMAND_EVENT_SOURCE_SLOT: &str = "__moliCommandEventSource";
+const COMMAND_EVENT_COMMAND_SLOT: &str = "__moliCommandEventCommand";
 const EVENT_SUBCLASS_KIND_SLOT: &str = "__moliEventSubclassKind";
 const BEFORE_UNLOAD_EVENT_RETURN_VALUE_SLOT: &str = "__moliBeforeUnloadEventReturnValue";
 #[derive(WebApiObject)]
@@ -352,6 +354,18 @@ pub(crate) fn event_is_error_event<'s>(
     event_subclass_kind(scope, event) == Some(EventSubclassKind::ErrorEvent)
 }
 
+pub(crate) fn set_event_source_value<'s>(
+    scope: &mut v8::PinScope<'s, '_>,
+    event: v8::Local<'s, v8::Object>,
+    value: v8::Local<'s, v8::Value>,
+) {
+    if event_subclass_kind(scope, event) == Some(EventSubclassKind::CommandEvent) {
+        set_private_value(scope, event, COMMAND_EVENT_SOURCE_SLOT, value);
+    } else {
+        let _ = event.set(scope, v8str(scope, "source").into(), value);
+    }
+}
+
 pub(crate) fn event_is_mouse_event<'s>(
     scope: &mut v8::PinScope<'s, '_>,
     event: v8::Local<'s, v8::Object>,
@@ -519,6 +533,34 @@ pub(super) fn submit_event_submitter_getter_function<'s>(
 ) {
     let value = get_private_value(scope, args.this(), SUBMIT_EVENT_SUBMITTER_SLOT)
         .unwrap_or_else(|| v8::null(scope).into());
+    rv.set(value);
+}
+
+pub(super) fn command_event_source_getter_function<'s>(
+    scope: &mut v8::PinScope<'s, '_>,
+    args: v8::FunctionCallbackArguments<'s>,
+    mut rv: v8::ReturnValue<'_, v8::Value>,
+) {
+    if event_subclass_kind(scope, args.this()) != Some(EventSubclassKind::CommandEvent) {
+        throw_type_error(scope, "Illegal invocation");
+        return;
+    }
+    let value = get_private_value(scope, args.this(), COMMAND_EVENT_SOURCE_SLOT)
+        .unwrap_or_else(|| v8::null(scope).into());
+    rv.set(value);
+}
+
+pub(super) fn command_event_command_getter_function<'s>(
+    scope: &mut v8::PinScope<'s, '_>,
+    args: v8::FunctionCallbackArguments<'s>,
+    mut rv: v8::ReturnValue<'_, v8::Value>,
+) {
+    if event_subclass_kind(scope, args.this()) != Some(EventSubclassKind::CommandEvent) {
+        throw_type_error(scope, "Illegal invocation");
+        return;
+    }
+    let value = get_private_value(scope, args.this(), COMMAND_EVENT_COMMAND_SLOT)
+        .unwrap_or_else(|| v8str(scope, "").into());
     rv.set(value);
 }
 

lexmount/moli

moli-renderer-v8/src/context_bootstrap/events.rs
@@ -15,6 +15,8 @@ const CLOSE_EVENT_REASON_SLOT: &str = "__moliCloseEventReason";
 const SUBMIT_EVENT_SUBMITTER_SLOT: &str = "__moliSubmitEventSubmitter";
 const FORM_DATA_EVENT_FORM_DATA_SLOT: &str = "__moliFormDataEventFormData";
 const TRACK_EVENT_TRACK_SLOT: &str = "__moliTrackEventTrack";
+const COMMAND_EVENT_SOURCE_SLOT: &str = "__moliCommandEventSource";
+const COMMAND_EVENT_COMMAND_SLOT: &str = "__moliCommandEventCommand";
 const EVENT_SUBCLASS_KIND_SLOT: &str = "__moliEventSubclassKind";
 const BEFORE_UNLOAD_EVENT_RETURN_VALUE_SLOT: &str = "__moliBeforeUnloadEventReturnValue";
 #[derive(WebApiObject)]
@@ -359,6 +361,19 @@ pub(crate) fn event_is_error_event<'s>(
     event_subclass_kind(scope, event) == Some(EventSubclassKind::ErrorEvent)
 }
 
+pub(crate) fn set_event_source_value<'s>(
+    scope: &mut v8::PinScope<'s, '_>,
+    event: v8::Local<'s, v8::Object>,
+    value: v8::Local<'s, v8::Value>,
+) {
+    let event = event_backing(scope, event);
+    if event_subclass_kind(scope, event) == Some(EventSubclassKind::CommandEvent) {
+        set_private_value(scope, event, COMMAND_EVENT_SOURCE_SLOT, value);
+    } else {
+        let _ = event.set(scope, v8str(scope, "source").into(), value);
+    }
+}
+
 pub(crate) fn event_is_mouse_event<'s>(
     scope: &mut v8::PinScope<'s, '_>,
     event: v8::Local<'s, v8::Object>,
@@ -544,6 +559,39 @@ pub(super) fn submit_event_submitter_getter_function<'s>(
     if let Some(value) = super::platform_object_worlds::in_realm(scope, value, context) {
         rv.set(value);
     }
+}
+
+pub(super) fn command_event_source_getter_function<'s>(
+    scope: &mut v8::PinScope<'s, '_>,
+    args: v8::FunctionCallbackArguments<'s>,
+    mut rv: v8::ReturnValue<'_, v8::Value>,
+) {
+    if event_subclass_kind(scope, args.this()) != Some(EventSubclassKind::CommandEvent) {
+        throw_type_error(scope, "Illegal invocation");
+        return;
+    }
+    let value = event_private_value(scope, args.this(), COMMAND_EVENT_SOURCE_SLOT)
+        .unwrap_or_else(|| v8::null(scope).into());
+    let Some(context) = args.this().get_creation_context(scope) else {
+        return;
+    };
+    if let Some(value) = super::platform_object_worlds::in_realm(scope, value, context) {
+        rv.set(value);
+    }
+}
+
+pub(super) fn command_event_command_getter_function<'s>(
+    scope: &mut v8::PinScope<'s, '_>,
+    args: v8::FunctionCallbackArguments<'s>,
+    mut rv: v8::ReturnValue<'_, v8::Value>,
+) {
+    if event_subclass_kind(scope, args.this()) != Some(EventSubclassKind::CommandEvent) {
+        throw_type_error(scope, "Illegal invocation");
+        return;
+    }
+    let value = event_private_value(scope, args.this(), COMMAND_EVENT_COMMAND_SLOT)
+        .unwrap_or_else(|| v8str(scope, "").into());
+    rv.set(value);
 }
 
 pub(super) fn form_data_event_form_data_getter_function<'s>(