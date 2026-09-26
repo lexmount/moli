Stravia-AI/moli-stealth

fix(events): retarget sources across composed dispatch

moli-renderer-v8/src/context_bootstrap/events.rs
@@ -16,6 +16,7 @@ const FORM_DATA_EVENT_FORM_DATA_SLOT: &str = "__moliFormDataEventFormData";
 const TRACK_EVENT_TRACK_SLOT: &str = "__moliTrackEventTrack";
 const COMMAND_EVENT_SOURCE_SLOT: &str = "__moliCommandEventSource";
 const COMMAND_EVENT_COMMAND_SLOT: &str = "__moliCommandEventCommand";
+const TOGGLE_EVENT_SOURCE_SLOT: &str = "__moliToggleEventSource";
 const EVENT_SUBCLASS_KIND_SLOT: &str = "__moliEventSubclassKind";
 const BEFORE_UNLOAD_EVENT_RETURN_VALUE_SLOT: &str = "__moliBeforeUnloadEventReturnValue";
 #[derive(WebApiObject)]
@@ -359,10 +360,16 @@ pub(crate) fn set_event_source_value<'s>(
     event: v8::Local<'s, v8::Object>,
     value: v8::Local<'s, v8::Value>,
 ) {
-    if event_subclass_kind(scope, event) == Some(EventSubclassKind::CommandEvent) {
-        set_private_value(scope, event, COMMAND_EVENT_SOURCE_SLOT, value);
-    } else {
-        let _ = event.set(scope, v8str(scope, "source").into(), value);
+    match event_subclass_kind(scope, event) {
+        Some(EventSubclassKind::CommandEvent) => {
+            set_private_value(scope, event, COMMAND_EVENT_SOURCE_SLOT, value);
+        }
+        Some(EventSubclassKind::ToggleEvent) => {
+            set_private_value(scope, event, TOGGLE_EVENT_SOURCE_SLOT, value);
+        }
+        _ => {
+            let _ = event.set(scope, v8str(scope, "source").into(), value);
+        }
     }
 }
 
@@ -561,6 +568,20 @@ pub(super) fn command_event_command_getter_function<'s>(
     }
     let value = get_private_value(scope, args.this(), COMMAND_EVENT_COMMAND_SLOT)
         .unwrap_or_else(|| v8str(scope, "").into());
+    rv.set(value);
+}
+
+pub(super) fn toggle_event_source_getter_function<'s>(
+    scope: &mut v8::PinScope<'s, '_>,
+    args: v8::FunctionCallbackArguments<'s>,
+    mut rv: v8::ReturnValue<'_, v8::Value>,
+) {
+    if event_subclass_kind(scope, args.this()) != Some(EventSubclassKind::ToggleEvent) {
+        throw_type_error(scope, "Illegal invocation");
+        return;
+    }
+    let value = get_private_value(scope, args.this(), TOGGLE_EVENT_SOURCE_SLOT)
+        .unwrap_or_else(|| v8::null(scope).into());
     rv.set(value);
 }
 

lexmount/moli

moli-renderer-v8/src/context_bootstrap/events.rs
@@ -17,6 +17,7 @@ const FORM_DATA_EVENT_FORM_DATA_SLOT: &str = "__moliFormDataEventFormData";
 const TRACK_EVENT_TRACK_SLOT: &str = "__moliTrackEventTrack";
 const COMMAND_EVENT_SOURCE_SLOT: &str = "__moliCommandEventSource";
 const COMMAND_EVENT_COMMAND_SLOT: &str = "__moliCommandEventCommand";
+const TOGGLE_EVENT_SOURCE_SLOT: &str = "__moliToggleEventSource";
 const EVENT_SUBCLASS_KIND_SLOT: &str = "__moliEventSubclassKind";
 const BEFORE_UNLOAD_EVENT_RETURN_VALUE_SLOT: &str = "__moliBeforeUnloadEventReturnValue";
 #[derive(WebApiObject)]
@@ -367,10 +368,16 @@ pub(crate) fn set_event_source_value<'s>(
     value: v8::Local<'s, v8::Value>,
 ) {
     let event = event_backing(scope, event);
-    if event_subclass_kind(scope, event) == Some(EventSubclassKind::CommandEvent) {
-        set_private_value(scope, event, COMMAND_EVENT_SOURCE_SLOT, value);
-    } else {
-        let _ = event.set(scope, v8str(scope, "source").into(), value);
+    match event_subclass_kind(scope, event) {
+        Some(EventSubclassKind::CommandEvent) => {
+            set_private_value(scope, event, COMMAND_EVENT_SOURCE_SLOT, value);
+        }
+        Some(EventSubclassKind::ToggleEvent) => {
+            set_private_value(scope, event, TOGGLE_EVENT_SOURCE_SLOT, value);
+        }
+        _ => {
+            let _ = event.set(scope, v8str(scope, "source").into(), value);
+        }
     }
 }
 
@@ -592,6 +599,25 @@ pub(super) fn command_event_command_getter_function<'s>(
     let value = event_private_value(scope, args.this(), COMMAND_EVENT_COMMAND_SLOT)
         .unwrap_or_else(|| v8str(scope, "").into());
     rv.set(value);
+}
+
+pub(super) fn toggle_event_source_getter_function<'s>(
+    scope: &mut v8::PinScope<'s, '_>,
+    args: v8::FunctionCallbackArguments<'s>,
+    mut rv: v8::ReturnValue<'_, v8::Value>,
+) {
+    if event_subclass_kind(scope, args.this()) != Some(EventSubclassKind::ToggleEvent) {
+        throw_type_error(scope, "Illegal invocation");
+        return;
+    }
+    let value = event_private_value(scope, args.this(), TOGGLE_EVENT_SOURCE_SLOT)
+        .unwrap_or_else(|| v8::null(scope).into());
+    let Some(context) = args.this().get_creation_context(scope) else {
+        return;
+    };
+    if let Some(value) = super::platform_object_worlds::in_realm(scope, value, context) {
+        rv.set(value);
+    }
 }
 
 pub(super) fn form_data_event_form_data_getter_function<'s>(